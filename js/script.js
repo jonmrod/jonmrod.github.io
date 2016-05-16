08903c489d82dce9db31f99ca8b8163a3d594584
Jonathan Rodriguez <jonathanrodriguez224@gmail.com>
2016-05-16 16:04:30 -0400

    changed speed of elements

--- js/script.js
+++ js/script.js
@@ -11,23 +11,23 @@ $(document).ready(function() {
         width: '+=10',height: '+=10', opacity: 1});
         //animate text
         setTimeout(function() {
-            $('#textBody').animate({'font-size': '-=2pt', opacity: 1}, 500);
-        }, 500);
+            $('#textBody').animate({'font-size': '-=2pt', opacity: 1});
+        }, 600);
         setTimeout(function() {
-            $('#textBody p').animate({'font-size': '-=2pt', opacity: 1}, 500);
-        }, 1000);
+            $('#textBody p').animate({'font-size': '-=2pt', opacity: 1});
+        }, 700);
         //hr line
         setTimeout(function() {
-            $('hr').animate({width: '50%', opacity: 1}, 1000)
-        }, 1500);
+            $('hr').animate({width: '50%', opacity: 1})
+        }, 800);
         //delay background
         setTimeout(function() {
             $('html').css("background-image", "url('./icons/bg.png')");
         }, 2000);
         //link images
         setTimeout(function() {
-            $('#images img').animate({width: '25px', height: '25px', opacity: 1}, 500)
-        }, 2500);
+            $('#images img').animate({width: '25px', height: '25px', opacity: 1})
+        }, 900);
 
 
     }, 500);
